1e26d3b9df14b46e6409f63ddfb8c908396b1453
Pawan <pawan.yadav@infrasofttech.com>
2019-10-03 16:41:02 +0530

    Modified main method

--- src/main/java/DevPro/DevPro/App.java
+++ src/main/java/DevPro/DevPro/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Running main method" );
+        System.out.println( "Running cucumber features" );
     }
 }
